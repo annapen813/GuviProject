982af0028c949278a18ac1cb827b7ba8896112df
annapen813 <anna.padmanaban@gmail.com>
2023-01-25 15:46:23 +0400

    updated PrimeNumber

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -11,4 +11,17 @@ def getPrimeNumberSeries():
     else:
         print('Given value is not valid')  
 
+
+def isPrimeNumber():
+    x = int(input('Please enter the number to check if its a Prime Number : '))
+
+    if(x > 1):
+        if(x % 2 != 0 and x % 3 != 0 and x % 5 != 0 and x % 7 != 0 and x % 11 != 0):
+            print('Yes, the given number is a Prime Number')
+        else:
+            print('No, the given number is not a Prime Number')
+    else:
+        print('No, the given number is not a Prime Number')
+
 getPrimeNumberSeries()
+isPrimeNumber()
